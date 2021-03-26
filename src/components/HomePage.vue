c2e8e15d34d12538ba24c06821f7936bca0b3fcc
Emily Lu <emily.lu@aspencapital.com>
2021-03-26 13:10:25 -0400

    feature: bp frontend

---
+++ src/components/HomePage.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="home-page">
+    <div id="card">
+      <img id="company-img" src="../../src/assets/gregoryFunding.png" alt="">
+      <PlaceHolder :placeholders="placeholders"/>
+      <div class="reg-detail">
+        <div class="pw-q-div">
+           <a class="pw">Forgot password?</a><a class="security-q">ForgotSecurity question/answer?</a>
+        </div>
+        <a class="change-q">
+           Change security question/answer?
+        </a>
+      </div>
+      <div id="login">Login</div>
+      <div class="reg-detail pw-q-div bottom-reg">
+        <a>Register</a><a>Contact Us</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PlaceHolder from './PlaceHolder'
+
+export default {
+  props: {
+    msg: String
+  },
+  components:{
+    PlaceHolder
+  },
+  data() {
+   return {
+      placeholders:[
+        {
+          "title":"Email",
+          "content":""
+        },
+        {
+          "title":"Password",
+          "content":""
+        },
+        {
+          "title":"Select Security Question",
+          "content":""
+        },
+        {
+          "title":"Answer",
+          "content":""
+        }
+     ]
+  }
+ }
+}
+</script>
+
+<style scoped>
+  .home-page{
+    width:20%;
+    margin: auto;
+    font-size:14px;
+    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif;
+    font-weight: 100;
+  }
+  #card{
+    border: 1px solid rgba(0,0,0,0.125);
+    padding: 25px 0;
+  }
+
+  #company-img{
+    width: 90%;
+  }
+
+  #login{
+    width: 86%;
+    margin: auto;
+    padding: 8px 8px;
+    border: 1px solid #B47804;
+    background: #B47804;
+    color: white;
+  }
+
+  #login:hover{
+      color:#B47804;
+      background: white;
+      cursor: pointer;
+    }
+
+.reg-detail{
+     color:#B47804;
+     padding: 15px 0;
+  }
+.pw-q-div{
+    display:flex;
+    justify-content: space-between;
+    width: 89%;
+    margin: auto;
+}
+.change-q{
+    display: flex;
+    justify-content: flex-end;
+    margin: 12px 21px;
+}
+.bottom-reg{
+    padding-top: 55px;
+}
+a:hover{
+    text-decoration: underline;
+    cursor: pointer;
+}
+</style>
---
+++ src/components/PlaceHolder.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="place-holder">
+        <div class="input-div" v-for="(placeholder, index) in placeholders" :key="index">
+            <input class="input" :placeholder="placeholder.title" v-model="placeholder.content">
+            <!-- <input v-if="placeholder.title !== `Select Security Question`"  class="input" :placeholder="placeholder.title" v-model="placeholder.content"> -->
+            <!-- <input v-else class="input input-question" :placeholder="selectedQue" :click="toggleDropdown"> -->
+            <!-- <img id="arrow-down" v-if= "placeholder === 'Select Security Question'" src="../assets/dropdown.png" alt="">
+            <div id="dropdown-div" v-show="isShow && placeholder.title === 'Select Security Question'" >
+                <div v-for="(que, index) in securityQ" :key="index" :click="selectQ(que)">{{que}}</div>
+            </div> -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        placeholders:Array
+    },
+    data() {
+        return {
+            isShow:true,
+            selectedQ:"Select Security Question",
+            securityQ:["Select Security Question","What was the name of your pet?", "What year did you graduate from high school?",
+                       "In what city were you born?", "What street did you live on in third grade?", "What was the last name of your third grade teacher?",
+                       "Who is your favorite sports team?"]
+        }
+    },
+    computed:{
+        selectedQue () {
+            return this.selectedQ
+        }
+    },
+    // methods: {
+    //     toggleDropdown() {
+    //         this.isShow = !this.isShow
+    //     },
+    //     selectQ (que) {
+    //         this. selectedQ = que
+    //         this.isShow = false
+    //     }
+    // }
+
+}
+</script>
+
+<style scoped>
+    .place-holder{
+        margin: auto;
+    }
+    .input-div{
+        padding: 15px 0;
+        position: relative;
+    }
+    .input{
+        width: 86%;
+        padding: 8px 8px;
+        border-radius: 4px;
+        border: 1px solid rgba(0,0,0,0.125);
+    }
+
+    #arrow-down{
+        position: absolute;
+        width: 15px;
+        right: 6%;
+        top: 37%;
+    }
+
+    #dropdown-div{
+        position: absolute;
+        background: white;
+        border: 1px solid rgba(0,0,0,0.125);
+        top: 75%;
+        left: 5%;
+        z-index: 1;
+        font-weight: 400;
+    }
+    #dropdown-div div{
+        padding: 8px 8px;
+        text-align: left;
+    }
+    #dropdown-div div:hover{
+      background: #ababfb;
+      color: white;
+      cursor: pointer;
+    }
+
+</style>
